d27e158e0612755fd4cfd35a256decad5994538c
root <root@fortiapi.bcfa.co.za>
2023-12-05 15:41:03 +0000

    Revert "Revert "New Services Added: Revert Interface""
    This reverts commit 8d0ea436abd53d6bdab282b458b541d54ec47252.

--- fortiwan_services/urls.py
+++ fortiwan_services/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path('check-for-token/', fortiauthenticator_api_services.status_token, name='status_token'),
     path('ipsec-vpn-foc-monitor/', fortiauthenticator_api_services.get_ipsec, name='get_ipsec'),
     path('ipsec-vpn-foc-configure/', fortiauthenticator_api_services.post_interface_switch, name='switch_interface'),
-    # path('tunnel-overview/', fortiauthenticator_api_services.post_interface_switch, name='tunnel_overview'),
-
+    path('ipsec-vpn-foc-revert/', fortiauthenticator_api_services.revert_interface, name='revert_interface'),
 ]
